a18687863d88719a873d46201c60bde90b02262d
Lucas Garcia <lucas8bernadochi@gmail.com>
2024-09-04 17:52:26 -0300

    implementation of the Seller class and test on main Program.

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,12 +1,17 @@
 package Application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "forest");
+        Seller seller = new Seller(3, "lucas", "lucas8bernadochi@gmail.com", new Date(), 2000.0, obj);
         System.out.println(obj);
+        System.out.println(seller);
 
     }
 }
